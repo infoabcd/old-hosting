d251595cba9296aaa5eb04c3e304e0f1d0bd7287
DouZi <57176918+infoabcd@users.noreply.github.com>
2020-12-07 08:18:40 -0800

    Update common.php

--- var/www/common.php
+++ var/www/common.php
@@ -56,8 +56,11 @@ opcache.optimization_level = 0x7fffffff
 opcache.validate_permission = 1
 opcache.validate_root = 1
 <<<<<<< HEAD
+<<<<<<< HEAD
 opcache.jit_buffer_size = 64M
 session.use_strict_mode = 1
+=======
+>>>>>>> parent of 67cea03... Enable strict session id mode
 =======
 >>>>>>> parent of 67cea03... Enable strict session id mode
 ';
